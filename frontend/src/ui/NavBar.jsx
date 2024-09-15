8d471f9b944910bc7df7073441a9a5817c2cc088
Marce Quadrilatero <marceloquadrilatero@gmail.com>
2024-09-15 18:42:01 -0300

    update front new hero scroll

--- frontend/src/ui/NavBar.jsx
+++ frontend/src/ui/NavBar.jsx
@@ -1,24 +1,34 @@
-import "../style/navbar.scss"
-
-
-
+import "../style/navbar.scss";
 
 export const NavBar = () => {
   return (
     <section className="navbar">
-        <div className="cont-navbar">
-            <h1 className="titulo-navbar">Limonada.</h1>
-            <ul className="ul-navbar-redes-sociales">
-                <li><i className="fa-brands fa-x-twitter"></i></li>
-                <li><i className="fa-brands fa-instagram"></i></li>
-                <li><i className="fa-brands fa-tiktok"></i></li>
-            </ul>
+      <div className="cont-navbar">
+        <h1 className="titulo-navbar">Limonada.</h1>
+        <ul className="ul-navbar-redes-sociales">
+          <li>
+            <i className="fa-brands fa-x-twitter"></i>
+          </li>
+          <li>
+            <i className="fa-brands fa-instagram"></i>
+          </li>
+          <li>
+            <i className="fa-brands fa-tiktok"></i>
+          </li>
+        </ul>
+      </div>
+      <div className="cont-scroll-data">
+
+
+        <div className="cont-moving">
+          <img src="https://i.pinimg.com/736x/87/da/7c/87da7cf2830b2c2c6378f8d596a35ba7.jpg" alt="img scroll" className="img-scroll-data"/>
         </div>
-        <div className="opciones-catalogo-navbar">
-          <p>Cine</p>
-          <p>Anime</p>
-          <p>Bandas</p>
-        </div>
+      </div>
+      <div className="opciones-catalogo-navbar">
+        <p>Cine</p>
+        <p>Anime</p>
+        <p>Bandas</p>
+      </div>
     </section>
-  )
-}
+  );
+};
